3a7acea8c51f207765effd4ff469f6970b0aaf15
Leon Chou <iamyiii0328@gmail.com>
2025-04-22 03:44:56 +0800

    fix: 修復路徑

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use('/api/message', MessageRoutes)
 
 if (process.env.NODE_ENV === 'prod') {
   // 設定靜態文件路徑
-  app.use(express.static(path.join(__dirname, 'frontend', 'dist')))
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
   // 處理所有路由
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
